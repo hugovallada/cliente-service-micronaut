bf5c8fe466379f94a52efd435ff448072f551728
Hugo Vallada <valladahugo@gmail.com>
2021-09-25 18:58:48 -0300

    Adiciona o método de delete e findById

--- src/main/kotlin/com/github/hugovallada/controller/ClienteController.kt
+++ src/main/kotlin/com/github/hugovallada/controller/ClienteController.kt
@@ -2,9 +2,12 @@ package com.github.hugovallada.controller
 
 import com.github.hugovallada.model.Cliente
 import com.github.hugovallada.repository.ClienteRepository
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Delete
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.exceptions.HttpStatusException
 
 @Controller("/clientes")
 class ClienteController(private val repository: ClienteRepository) {
@@ -17,6 +20,22 @@ class ClienteController(private val repository: ClienteRepository) {
     @Get
     fun findAll() : List<Cliente> {
         return repository.findAll();
+    }
+
+    @Delete("{id}")
+    fun delete(id: Long) {
+        repository.findById(id).run {
+            if(isPresent) repository.delete(get())
+        }
+    }
+
+    @Get("{id}")
+    fun findById(id: Long) : Cliente {
+        repository.findById(id).run {
+            if(isPresent) return get()
+        }
+
+        throw HttpStatusException(HttpStatus.BAD_REQUEST, "Bad Request")
     }
 
 }
