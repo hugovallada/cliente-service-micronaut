6530919c921f2d00eea5173b0139fc25865c0955
Hugo Vallada <valladahugo@gmail.com>
2021-10-02 17:38:23 -0300

    Substitui o HttpResponse por @Status e produces, Adiciona o @ExecuteOn para rodar de forma blocante sem forçar o netty

--- src/main/kotlin/com/github/hugovallada/controller/ClienteController.kt
+++ src/main/kotlin/com/github/hugovallada/controller/ClienteController.kt
@@ -4,36 +4,44 @@ import com.github.hugovallada.model.Cliente
 import com.github.hugovallada.service.ClienteService
 import io.micronaut.data.model.Page
 import io.micronaut.data.model.Pageable
-import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.*
+import io.micronaut.scheduling.TaskExecutors
+import io.micronaut.scheduling.annotation.ExecuteOn
 
-@Controller("/clientes")
+@Controller("/clientes", produces = [MediaType.APPLICATION_JSON])
+@ExecuteOn(TaskExecutors.IO)
 class ClienteController(private val service: ClienteService) {
 
     @Post
-    fun create(cliente: Cliente): HttpResponse<Any> = HttpResponse.created(service.create(cliente))
+    @Status(HttpStatus.CREATED)
+    fun create(cliente: Cliente): Cliente {
+        service.create(cliente)
+        return cliente
+    }
 
     @Get
-    fun findAll(page: Pageable): HttpResponse<Page<Cliente>> = HttpResponse.ok(service.findAll(page))
+    fun findAll(page: Pageable): Page<Cliente> = service.findAll(page)
 
     @Delete("{id}")
-    fun delete(id: Long): HttpResponse<Any> {
+    @Status(HttpStatus.NO_CONTENT)
+    fun delete(id: Long) {
         service.delete(id)
-        return HttpResponse.noContent()
     }
 
     @Get("{id}")
-    fun findById(id: Long): HttpResponse<Cliente> = HttpResponse.ok(service.findById(id))
+    fun findById(id: Long): Cliente = service.findById(id)
 
     @Put("{id}")
-    fun update(id: Long, cliente: Cliente) : HttpResponse<Any> {
+    @Status(HttpStatus.ACCEPTED)
+    fun update(id: Long, cliente: Cliente) {
         service.update(id, cliente)
-        return HttpResponse.accepted()
     }
 
     @Get("/names")
-    fun findByName(@QueryValue name: String) : HttpResponse<Cliente> {
-        return HttpResponse.ok(service.findByName(name))
+    fun findByName(@QueryValue name: String): Cliente {
+        return service.findByName(name)
     }
 
 }
